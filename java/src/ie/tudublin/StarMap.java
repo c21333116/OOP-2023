f38c38e4ddb19b96696a9400102482e8a408fb24
C21333116 Shahida Mohammed-Ahmed <c21333116@mytudublin.ie>
2023-02-20 12:22:25 +0000

    audio1

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -10,16 +10,39 @@ public class StarMap extends PApplet
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		colorMode(RGB);
 		background(0);
 
 		smooth();
 		
 	}
 
+	public void DrawGrid()
+	{
+		stroke(0, 255, 255);
+		float border = width * 0.1f;
+		
+		int count = 10;
+		float gap = (width - (border * 2.0f)) / (float) count;
+		for (int i = -5; i <=5; i++)
+		{
+			float x = border + (gap * (i + 5));
+			line(x, border, x, height-border);
+			line(border, x, width-border, x);
+
+			textAlign(CENTER, CENTER);
+			text(i, x + (gap * 0.5f), gap * 0.5f);
+
+		}
+	}
 		
 	public void draw()
 	{	
-		strokeWeight(2);		
+		strokeWeight(2);
+		DrawGrid();
+		
+
 	}
+
+	
 }
